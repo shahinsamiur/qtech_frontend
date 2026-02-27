8697f94203e58a2da3d78479afda15260d9b5f4b
.... <shahinsamiur@yahoo.com>
2026-02-28 04:15:52 +0600

    feat: jobcard components added

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,5 +1,24 @@
-import React from "react";
-
+import JobCard from "./jobCard";
 export default function Hero() {
-  return <div className="w-full min-h-screen">Hero</div>;
+  const arr: Array<number> = [1, 2, 3, 4, 5, 6, 7];
+  return (
+    <div className="w-full min-h-screen text-red-500 py-20 max-sm:pt-30">
+      <section>
+        <div className="w-full flex flex-wrap md:space-x-4 space-y-4 justify-center md:justify-start items-center">
+          {arr.map((item, index) => (
+            <JobCard
+              key={index}
+              id="1"
+              title="Product Designer"
+              company="ClassPass"
+              location="Manchester, UK"
+              description="ClassPass is looking for Product Designer to help us build beautiful experiences..."
+              type="Full Time"
+              categories={["Marketing", "Design"]}
+            />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 }
---
+++ components/jobCard.tsx
@@ -0,0 +1,82 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export interface JobCardProps {
+  id: string;
+  title: string;
+  company: string;
+  location: string;
+  description: string;
+  type: "Full Time" | "Part Time" | "Remote" | string;
+  categories: string[];
+  logoUrl?: string;
+  onClick?: () => void;
+}
+
+const JobCard: React.FC<JobCardProps> = ({
+  title,
+  company,
+  location,
+  description,
+  type,
+  categories,
+  logoUrl,
+  //   onClick,
+}) => {
+  return (
+    <Link
+      href={`/job/`}
+      //   onClick={onClick}
+      className="bg-white w-full md:w-[45%]  lg:w-[23.5%] space-y-4 shadow-sm border border-[#D6DDEB] p-4 px-6 hover:shadow-sm transition duration-300 cursor-pointer"
+    >
+      <div className="flex items-start  justify-between">
+        <div className="flex items-center gap-4 justify-between w-full">
+          <div className="w-12 h-12 rounded-full bg-blue-100 flex items-center justify-center overflow-hidden">
+            {logoUrl ? (
+              <Image
+                src={logoUrl}
+                alt={company}
+                width={900}
+                height={900}
+                className="w-full h-full object-cover"
+              />
+            ) : (
+              <span className="text-blue-600 font-semibold text-md">
+                {company.charAt(0)}
+              </span>
+            )}
+          </div>
+
+          <span className="text-sm font-medium text-blue-600 border border-blue-500 px-3 py-1 ">
+            {type}
+          </span>
+        </div>
+      </div>
+      <div>
+        <h3 className="text-lg font-medium text-gray-800">{title}</h3>
+        <p className="text-sm text-gray-500">
+          {company} • {location}
+        </p>
+      </div>
+      <p className="mt-4 text-sm text-gray-600 line-clamp-2">{description}</p>
+
+      <div className="mt-4 flex gap-3 flex-wrap">
+        {categories.map((cat, index) => (
+          <span
+            key={index}
+            className={`text-xs px-4 py-1 rounded-full font-medium ${
+              index % 2 === 0
+                ? "bg-[#EB85331A] text-[#FFB836]"
+                : "bg-[#56CDAD1A] text-[#56CDAD]"
+            }`}
+          >
+            {cat}
+          </span>
+        ))}
+      </div>
+    </Link>
+  );
+};
+
+export default JobCard;
